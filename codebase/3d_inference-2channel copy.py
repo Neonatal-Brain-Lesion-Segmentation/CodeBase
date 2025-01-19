3027c3c6daf24105aa7c63de4dfc73a8a1e569c7
amograo24 <amograo24@gmail.com>
2025-01-19 23:53:12 +0530

    save all slices

--- codebase/3d_inference-2channel copy.py
+++ codebase/3d_inference-2channel copy.py
@@ -12,23 +12,6 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-def save_all_slices(gt_masks, pred_masks, uid):
-    for i in range(gt_masks.shape[0]):
-        fig, axes = plt.subplots(1, 2, figsize=(10, 5))
-        
-        axes[0].imshow(gt_masks[i], cmap='gray')
-        axes[0].set_title(f'GT Mask - Slice {i}')
-        axes[0].axis('off')
-        
-        axes[1].imshow(pred_masks[i], cmap='gray')
-        axes[1].set_title(f'Predicted Mask - Slice {i}')
-        axes[1].axis('off')
-        
-        plt.tight_layout()
-        plt.savefig(f'/Users/amograo/Desktop/3C/{uid}_slice_{i}.png')
-        plt.close()
-
-
 def show_all_slices_in_grid(gt_masks, pred_masks, pairs_per_row=4):
     """
     Display all GT Mask and Predicted Mask slices in a grid with specified pairs per row.
@@ -65,6 +48,23 @@ def show_all_slices_in_grid(gt_masks, pred_masks, pairs_per_row=4):
     plt.tight_layout()
     plt.show()
 
+# make a function to save all image pairs individually
+def save_all_slices(gt_masks, pred_masks, uid):
+    for i in range(gt_masks.shape[0]):
+        fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+        
+        axes[0].imshow(gt_masks[i], cmap='gray')
+        axes[0].set_title(f'GT Mask - Slice {i}')
+        axes[0].axis('off')
+        
+        axes[1].imshow(pred_masks[i], cmap='gray')
+        axes[1].set_title(f'Predicted Mask - Slice {i}')
+        axes[1].axis('off')
+        
+        plt.tight_layout()
+        plt.savefig(f'/Users/amograo/Desktop/OneSeventy/{uid}_slice_{i}.png')
+        plt.close()
+
 DEVICE = "cuda" if torch.cuda.is_available() else "mps" if torch.mps.is_available() else "cpu"
 print(DEVICE)
 # ENCODER = "se_resnext101_32x4d"
@@ -78,7 +78,7 @@ print(ENCODER)
 model = smp.UnetPlusPlus(
     encoder_name=ENCODER,
     encoder_weights=None,
-    in_channels=3,
+    in_channels=2,
     classes=1,
     activation='sigmoid',
     decoder_attention_type='scse'
@@ -87,18 +87,9 @@ model = smp.UnetPlusPlus(
 
 model.to(DEVICE)
 # 125, 
-checkpoint = torch.load("/Users/amograo/Desktop/HIE-BONBID-24/UPP-3C-inceptionv4-TLHF-D0-Winit-NewSplit/models/model_epoch_204_inceptionv4_3d.pth",map_location=torch.device(DEVICE))
-# checkpoint = torch.load("/Users/amograo/Desktop/HIE-BONBID-24/UPPr-inceptionv4-TLHF-D1/models/model_epoch_170_inceptionv4_3d.pth", map_location=torch.device(DEVICE)) 
-# checkpoint = torch.load("/Users/amograo/Desktop/HIE-BONBID-24/UNetPlusPlus-inceptionv4-ResizeAug-SCSE-TLHF/models/model_epoch_95_inceptionv4.pth",map_location=torch.device(DEVICE))  
-# checkpoint = torch.load("/Users/amograo/Desktop/HIE-BONBID-24/UPP-inceptionv4-Aug-Stacked/models/model_epoch_191_inceptionv4_3d.pth",map_location=torch.device(DEVICE))
+# checkpoint = torch.load("/Users/amograo/Desktop/HIE-BONBID-24/UPP-3C-inceptionv4-TLHF-D0-Winit-NewSplit/models/model_epoch_204_inceptionv4_3d.pth",map_location=torch.device(DEVICE))
+checkpoint = torch.load("/Users/amograo/Desktop/HIE-BONBID-24/UPPr-inceptionv4-TLHF-D1/models/model_epoch_170_inceptionv4_3d.pth", map_location=torch.device(DEVICE)) 
 
-# checkpoint = torch.load("/Users/amograo/Desktop/HIE-BONBID-24/RECENT_SCRAPPED/UPP-se_resnext101_32x4d-Aug-Stacked/models/model_epoch_106_se_resnext101_32x4d_3d.pth",map_location=torch.device(DEVICE))
-# checkpoint = torch.load("/Users/amograo/Desktop/HIE-BONBID-24/UNetPlusPlus-densenet161-ResizeAug/models/model_epoch_178_densenet161_3d.pth",map_location=torch.device(DEVICE))   
-# checkpoint = torch.load("/Users/amograo/Desktop/HIE-BONBID-24/UNetPlusPlus-densenet161-ResizeAug-SCSE/models/model_epoch_69_densenet161_3d.pth",map_location=torch.device(DEVICE))   
-
-# checkpoint = torch.load("/Users/amograo/Desktop/HIE-BONBID-24/UNetPP-2D_densenet161-Aug-Stacked/models/model_epoch_235_densenet161_3d.pth",map_location=torch.device(DEVICE))   
-# checkpoint = torch.load("/Users/amograo/Desktop/submission/models/model_epoch_180_densenet161_3d.pth",map_location=torch.device(DEVICE))  
-# checkpoint = torch.load("/Users/amograo/Desktop/HIE-BONBID-24/TransUNet-3/models/model_epoch_101_TransUNet-3_3d.pth",map_location=torch.device(DEVICE))   
 model.load_state_dict(checkpoint['model_state_dict'])
 
 for name, param in model.named_parameters():
@@ -111,8 +102,8 @@ df = pd.read_csv(f'{DATA_ROOT}/{TV}/metadata.csv')
 # df = pd.read_csv(f'{DATA_ROOT}/metadata_val.csv')
 # column Lesion Percentatge must be less than 1.5 and greater than 0
 # df = df[(df['Lesion Percentage'] < 200) & (df['Lesion Percentage'] > 0)]
-# uids = [str(i).zfill(3) for i in df["Patient ID"].tolist()]
-uids = ['002']
+uids = [str(i).zfill(3) for i in df["Patient ID"].tolist()]
+# uids = ['002', '302', '436', '001']
 print(uids)
 
 preds_3d = {i: [] for i in uids}
@@ -125,15 +116,15 @@ dice_l = []
 masd_l = []
 nsd_l = []
 model.eval()
-# image_paths = [f'{DATA_ROOT}/{TV}/ADC',f'{DATA_ROOT}/{TV}/Z_ADC'] #f'{DATA_ROOT}/{TV}/ADC',
-image_paths = [f'{DATA_ROOT}/Z_ADC',f'{DATA_ROOT}/Z_ADC',f'{DATA_ROOT}/ADC']
+image_paths = [f'{DATA_ROOT}/ADC',f'{DATA_ROOT}/Z_ADC'] #f'{DATA_ROOT}/{TV}/ADC',
+# image_paths = [f'{DATA_ROOT}/Z_ADC',f'{DATA_ROOT}/Z_ADC',f'{DATA_ROOT}/ADC']
 for uid in uids:
     image_set = [reassemble_to_3d(path, uid) for path in image_paths]
     # image_set = reassemble_to_3d(f'{DATA_ROOT}/{TV}
     #/Z_ADC', uid)
 
     for i in range(image_set[0].shape[0]):
-        image = np.expand_dims(transform_2d_inner(np.stack([image_set[j][i] for j in range(len(image_set))]),['ZADC','ZADC_Clip','ADC']),axis=0)
+        image = np.expand_dims(transform_2d_inner(np.stack([image_set[j][i] for j in range(len(image_set))]),['ADC','Z_ADC']),axis=0)
         # image = np.expand_dims(resample(np.stack([image_set[i]])),axis=0)
 
         image = torch.tensor(image).to(DEVICE)
@@ -157,8 +148,8 @@ for uid in uids:
         # print(np.array(preds_3d[uid]).shape)
         if len(preds_3d[uid]) == image_set[0].shape[0]:
             preds_3d[uid] = np.stack(preds_3d[uid])
-            print("Original Volume: ",brain_lesion_percentage(image_set[2],masks_3d[uid]))  
-            print("Predicted Volume: ",brain_lesion_percentage(image_set[2],preds_3d[uid]))  
+            print("Original Volume: ",brain_lesion_percentage(image_set[0],masks_3d[uid]))  
+            print("Predicted Volume: ",brain_lesion_percentage(image_set[0],preds_3d[uid]))  
             dice = monai.metrics.DiceMetric(include_background=True,ignore_empty=False)
             masd = monai.metrics.SurfaceDistanceMetric(include_background=False, symmetric = True)
             nsd = monai.metrics.SurfaceDiceMetric(include_background=False, distance_metric="euclidean", class_thresholds=[2])
